9c0f7239392637cdadcf581948770678e746f869
zamuca303 <31083326+zamuca303@users.noreply.github.com>
2018-09-18 17:46:17 -0300

    Update Eleitor.java

--- Eleitor.java
+++ Eleitor.java
@@ -1,4 +1,20 @@
-
 public class Eleitor {
-
+	private String nome = new String();
+	private int nTitulo;
+	
+	public String getnome() {
+		return nome;
+	}
+	
+	public int getnTitulo() {
+		return nTitulo;
+	}
+	
+	public void setnome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setnTitulo(int nTitulo) {
+		this.nTitulo = nTitulo;
+	}
 }
